c6385ef70c0fbf7c4e53efd37b202ae4cbaa2c2a
Benedikt_MBP <provers.0-pluses@icloud.com>
2024-04-09 23:29:12 +0000

    bugfixes

--- src/main/main.ino
+++ src/main/main.ino
@@ -42,11 +42,6 @@ void setup() {
 
   Serial.println("Initialization completed.");
   
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(200);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(200);                       // wait for a second
-
 }
 void loop() {
   // Ensure WiFi is connected
